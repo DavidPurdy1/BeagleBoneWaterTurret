f335d14ecdcc5e8071015da8912421a43a5de4db
Beagle User <debian@BeagleBone.localdomain>
2023-02-16 21:34:10 +0000

    install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Installing pip..."
+sudo apt install pip3
+echo "Done ^.^"
+
+echo "Installing DLIB deb packages..."
+sudo apt-get install build-essential cmake pkg-config
+sudo apt-get install libx11-dev libatlas-base-dev
+sudo apt-get install libgtk-3-dev libboost-python-dev
+echo "Done :)"
+
+echo "Pip install the python packages..."
+pip3 install scipy
+pip3 install imutils
+pip3 install numpy
+pip3 install dlib # Needs other packages
+pip3 install open-cv2
+echo "Yippee it's done!" 
+
